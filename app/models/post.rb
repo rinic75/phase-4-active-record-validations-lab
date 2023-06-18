90de2948e0304f6f816e8e35b666636eadbf60d5
Chang Shin <cshin7374@gmail.com>
2023-06-17 20:13:33 -0500

    commit tree 4fc4d0e3fb0cd923f16643d59642fcde8f6087a4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: {maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbait?
+
+  def clickbait?
+    if title
+      unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top [number]") || title.include?("Guess")
+        errors.add(:title, "must be clickbait")
+      end
+    else
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
